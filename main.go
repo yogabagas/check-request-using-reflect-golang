c1253df2b73b38f593ddf233f1054b5cca01999f
Admin <yoga.bagasakthi@bluebirdgroup.com>
2020-07-31 13:56:03 +0700

    add testing

--- main.go
+++ main.go
@@ -9,13 +9,14 @@ type Request struct {
 	Long, Wide, High float64
 }
 
-func (r *Request) Cube() (resp float64, err error) {
+func (r *Request) Cube() float64 {
 
 	allow := r.validatorRequest()
 	if !allow {
-		return 0.0, err
+		fmt.Println("Not Allowed")
+		return 0.0
 	}
-	return r.Long * r.Wide * r.High, nil
+	return r.Long * r.Wide * r.High
 }
 
 func (r *Request) validatorRequest() bool {
@@ -45,15 +46,22 @@ func (r *Request) validatorRequest() bool {
 }
 
 func main() {
-	req := Request{
+	req1 := Request{
 		Long: 0,
 		Wide: 15.0,
 		High: 5.00,
 	}
-	resp, err := req.Cube()
-	if err != nil {
-		return
+
+	resp1 := req1.Cube()
+	fmt.Println("Result of Cube, If Request has an empty value", resp1)
+
+	req2 := Request{
+		Long: 10.0,
+		Wide: 20.0,
+		High: 15.0,
 	}
-	fmt.Println("HASIL", resp)
+
+	resp2 := req2.Cube()
+	fmt.Println("Result of Cube, If Request is fulfilled", resp2)
 
 }
